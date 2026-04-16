321162f150b32e1d44696dbacbdc8ae2b3588932
Nate Graves <nategraves29@gmail.com>
2026-04-16 15:59:19 -0700

    Initialize Firebase and export Firestore instance
    Export Firestore database instance after initialization.

--- src/firebase.js
+++ src/firebase.js
@@ -1,9 +1,6 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyBzahuQPThYJLkIx9ZSIQhOlJK-5Ls_d5o",
   authDomain: "garden-planner-2026.firebaseapp.com",
@@ -13,5 +10,5 @@ const firebaseConfig = {
   appId: "1:870905095059:web:4750697145cf99e1defe66"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
+export const db = getFirestore(app);
